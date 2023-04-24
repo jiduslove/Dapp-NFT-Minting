3c77adbc05e765ce556fb31f75ebf2a8d4a33dd0
jiduslove <oondayr@gamil.com>
2023-04-24 21:40:10 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+import Web3 from "web3";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./web3.config";
+
+const web3 = new Web3(window.ethereum);
+const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  const [account, setAccount] = useState("");
+
+  const onClickAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onClickMint = async () => {
+    try {
+      const mintNft = await contract.methods.mintNft().send({ from: account });
+      console.log(mintNft);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      {account ? (
+        <div>
+          {account.substring(0, 4)}...
+          {account.substring(account.length - 4)}
+          <button onClick={onClickMint}>민팅</button>
+        </div>
+      ) : (
+        <button onClick={onClickAccount}>지갑로그인</button>
+      )}
+    </div>
+  );
 }
 
 export default App;
